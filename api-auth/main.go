24c6623265b3d543d0f5ebd17d59b451956e23ff
Mathis Bois <mathis.bois@outlook.fr>
2022-06-14 10:56:05 +0200

    [Env] api-auth port: put api-auth port parameter in environement variable

--- api-auth/main.go
+++ api-auth/main.go
@@ -3,14 +3,51 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"strconv"
 
 	pb "github.com/trixky/hypertube/api/proto"
 	"github.com/trixky/hypertube/api/server"
 	"google.golang.org/grpc"
 )
 
+const (
+	env_port = "API_AUTH_PORT"
+)
+
+type Env struct {
+	Port int
+}
+
+func (e *Env) GetAll() {
+	const (
+		port_max = 65535
+		port_min = 1000
+	)
+
+	port, err := strconv.Atoi(os.Getenv(env_port))
+
+	if err != nil {
+		log.Fatalf("%s environement variable is corrupted or missing", env_port)
+	}
+
+	if port < port_min || port > port_max {
+		log.Fatalf("port need to be included between %d and %d (%d)", port_min, port_max, port)
+	}
+
+	e.Port = port
+}
+
+func readEnv() (env Env) {
+	env.GetAll()
+
+	return
+}
+
 func main() {
-	addr := "0.0.0.0:5051"
+	env := readEnv()
+
+	addr := "0.0.0.0:" + strconv.Itoa(env.Port)
 
 	listen, err := net.Listen("tcp", addr)
 
