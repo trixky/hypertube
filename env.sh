d5736c94f616ba959cb42db20aaad85b9a9dda4a
trixky <mabois@student.42.fr>
2022-06-14 18:26:25 +0200

    [Env] sh to .env: move exportation from .sh in dedicated .env for each image

--- env.sh
+++ env.sh
@@ -1,25 +1,5 @@
-#!/bin/bash
-
-# postgres
-export POSTGRES_USER=hyper
-export POSTGRES_PASSWORD=1234
-export POSTGRES_DB=hypertube
-export POSTGRES_HOST=postgres
-export POSTGRES_PORT=5432
-export PGUSER=$POSTGRES_USER
-export PGPASSWORD=$POSTGRES_PASSWORD
-export PGDATABASE=$POSTGRES_DB
-
-# pgadmin
-export PGADMIN_DEFAULT_EMAIL=admin@hypertube.com
-export PGADMIN_DEFAULT_PASSWORD=1234
-
-# # supertest
-# export PGHOST=$POSTGRES_HOST
-# export PGPORT=$
-
-# api-auth
-export API_AUTH_PORT=7010
-
-# client
-export CLIENT_PORT=4040
+for FOLDER in api-auth client pg-admin postgres
+do
+    echo "source $FOLDER"
+    export $(grep -v '^#' ./$FOLDER/.env | xargs)
+done
