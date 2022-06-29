6c509ad5aa3d84605def6ef8cd9e702d7a7ab9d7
trixky <mabois@student.42.fr>
2022-06-29 22:42:34 +0200

    [Dev] oauth2 buttons: add animation on oauth2 buttons

--- client/src/routes/login/continue-with.svelte
+++ client/src/routes/login/continue-with.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <!-- ========================= HTML -->
-<button class="inline-block bg-white py-2 px-3 rounded-full">
+<button class="inline-block bg-white py-2 px-3 rounded-full duration-[0.35s] hover:bg-gray-300">
 	Continue with
 	<img class="inline-block ml-[1px]" alt="{destination} logo" src="{logo}" height="20" width="20"/>
 </button>
