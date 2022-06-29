ce1ad764f5f72c72393f5074c7440377356a1200
trixky <mabois@student.42.fr>
2022-06-29 16:58:15 +0200

    [Dev] registration: finish the registration page design

---
+++ client/src/components/animations/spinner.svelte
@@ -0,0 +1,36 @@
+<!-- ========================= HTML -->
+<!-- https://loading.io/spinner/progress-pie/-circle-round-pie-progress-percent-ratio-pizza-color-palette -->
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	class="inline-block"
+	width="20px"
+	height="20px"
+	viewBox="0 0 100 100"
+	preserveAspectRatio="xMidYMid"
+>
+	<path
+		fill="none"
+		d="M50 30A20 20 0 0 1 50 70A20 20 0 0 1 50 30"
+		stroke="#000000"
+		stroke-width="40"
+	>
+		<animate
+			attributeName="stroke-dasharray"
+			repeatCount="indefinite"
+			dur="1.25"
+			values="0 0 0 126;0 0 126 126;0 126 126 126"
+			keyTimes="0;0.5;1"
+			keySplines="0.5 0 0.5 1;0.5 0 0.5 1"
+			calcMode="spline"
+		/>
+		<animate
+			attributeName="stroke"
+			values="#000000;#000000;#000000;#000000;#000000"
+			keyTimes="0;0.25;0.5;0.75;1"
+			dur="5s"
+			calcMode="discrete"
+			repeatCount="indefinite"
+		/>
+	</path>
+</svg>
