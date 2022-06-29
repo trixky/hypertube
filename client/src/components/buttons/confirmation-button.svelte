bafe5f2e72c182694b0064845c6c6e1e70dbd821
Mathis Bois <mathis.bois@outlook.fr>
2022-06-29 16:58:15 +0200

    [Dev] registration: finish the registration page design

---
+++ client/src/components/buttons/confirmation-button.svelte
@@ -0,0 +1,45 @@
+<!-- ========================= SCRIPT -->
+<script lang="ts">
+	import Spinner from '../animations/spinner.svelte';
+	import { fade } from 'svelte/transition';
+
+	export let name: string = '?';
+	let loading: boolean = false;
+
+	function toto() {
+		loading = !loading;
+	}
+</script>
+
+<!-- ========================= HTML -->
+<button
+	on:click|preventDefault={toto}
+	class:loading
+	class="relative mt-6 py-2 px-4 bg-blue-300 block m-auto rounded-sm duration-[0.35s]"
+>
+	{#if loading}
+		<div
+			in:fade={{ duration: 350, delay: 50 }}
+			out:fade={{ duration: 80 }}
+			class="spinner absolute inline-block left-4 opacity-0 transition-all delay-200 duration-[0.35s]"
+		>
+			<Spinner />
+		</div>
+	{/if}
+	<p class="inline-block">{name.charAt(0).toUpperCase() + name.slice(1)}</p>
+</button>
+
+<!-- ========================= CSS -->
+<style lang="postcss">
+	button.loading {
+		@apply pl-12 bg-blue-400;
+	}
+
+	button.loading > .spinner {
+		@apply opacity-100;
+	}
+
+	button:hover {
+		@apply bg-blue-400;
+	}
+</style>
--- client/src/routes/register/index@auth.svelte
+++ client/src/routes/register/index@auth.svelte
@@ -2,32 +2,36 @@
 <script lang="ts">
 	import BlackBox from '../../components/containers/black-box.svelte';
 	import Logo from '../../components/layouts/logo.svelte';
-	import External from './external.svelte';
 	import ConfirmationButton from '../../components/buttons/confirmation-button.svelte';
-
 </script>
 
 <!-- ========================= HTML -->
 <BlackBox>
 	<Logo alone />
-	<h1 class="mt-2 mb-1 text-2xl text-white">Login</h1>
-	<External />
-	<div>
-		<hr />
-		<p class="text-white inline-block">or</p>
-		<hr />
-	</div>
+	<h1 class="mt-2 mb-1 text-2xl text-white">Register</h1>
 	<form action="" class="pt-1">
+		<label for="username">Username</label>
+		<input type="text" placeholder="Username" name="username" />
+		<div class="flex justify-between">
+			<div class="pr-2">
+				<label for="firstname">Firstname</label>
+				<input type="text" placeholder="Firstname" name="firstname" />
+			</div>
+			<div class="pl-2">
+				<label for="lastname">Lastname</label>
+				<input type="text" placeholder="Lastname" name="lastname" />
+			</div>
+		</div>
 		<label for="email">Email</label>
 		<input type="email" placeholder="Email" name="email" />
 		<label for="password">Password</label>
 		<input type="password" placeholder="Password" name="password" minlength="8" />
-		<p class="extra-link mt-2 pl-28 mb-4 float-right"><a href="/register">Forgot your password ?</a></p>
-		<ConfirmationButton name="login"/>
-
+		<label for="confirm password">Confirm password</label>
+		<input type="password" placeholder="Confirm password" name="confirm password" minlength="8" />
+		<ConfirmationButton name="register"/>
 	</form>
 	<p class="extra-link mt-4">
-		<a href="/register">Not on Hypertube yet ? <span class="underline">Sign up</span></a>
+		<a href="/login">Already a member ? <span class="underline">Log in</span></a>
 	</p>
 </BlackBox>
 
@@ -39,10 +43,6 @@
 
 	input {
 		@apply w-full p-2 rounded-sm;
-	}
-
-	hr {
-		@apply border-white/60 inline-block w-28 -translate-y-1 mx-1;
 	}
 
 	label {
