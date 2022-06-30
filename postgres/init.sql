f2fd47c091001e8b41704266b1b9b8cb21384920
Glagan <Glagan@users.noreply.github.com>
2022-06-30 15:43:57 +0200

    [Dev] Update postgres scrapper tables

--- postgres/init.sql
+++ postgres/init.sql
@@ -22,35 +22,35 @@ VALUES (
 
 -- Scrapper
 
-CREATE TABLE medias (
+CREATE TABLE torrents (
     id BIGSERIAL PRIMARY KEY,
     full_url TEXT NOT NULL,
     imdb_title_id INTEGER NULL,
-    name VARCHAR (250) NULL,
-    type VARCHAR (50) NULL,
+    name VARCHAR (250) NOT NULL,
+    type VARCHAR (50) NOT NULL,
     seed INTEGER NULL,
     leech INTEGER NULL,
     size VARCHAR (250) NULL,
     upload_time TIMESTAMP WITH TIME ZONE NULL,
     description_html TEXT NULL,
-    torrent_url TEXT NOT NULL,
-    magnet TEXT NOT NULL,
-    imdb_id VARCHAR(10) NOT NULL
+    torrent_url TEXT NULL,
+    magnet TEXT NULL,
+    imdb_id VARCHAR(10) NULL
 );
 
-CREATE TABLE media_files (
+CREATE TABLE torrent_files (
     id BIGSERIAL PRIMARY KEY,
-    media_id INTEGER NOT NULL,
+    torrent_id INTEGER NOT NULL,
     path VARCHAR (250) NULL,
     name VARCHAR (250) NOT NULL,
     size VARCHAR (250) NULL
 );
-ALTER TABLE ONLY media_files
-ADD CONSTRAINT media_files_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY torrent_files
+ADD CONSTRAINT torrent_files_torrent_id_foreign FOREIGN KEY (torrent_id) REFERENCES torrents(id) ON DELETE CASCADE NOT DEFERRABLE;
 
 -- IMDB Informations
 
-CREATE TABLE imdb_titles (
+CREATE TABLE medias (
     id BIGSERIAL PRIMARY KEY,
     imdb_id VARCHAR (10) NOT NULL,
     description TEXT NULL,
@@ -62,32 +62,32 @@ CREATE TABLE imdb_titles (
     rating INTEGER NULL
 );
 
-CREATE TABLE imdb_title_names (
+CREATE TABLE media_names (
     id BIGSERIAL PRIMARY KEY,
-    imdb_title_id INTEGER NOT NULL,
+    media_id INTEGER NOT NULL,
     lang VARCHAR (5) NOT NULL,
     name VARCHAR (250) NOT NULL
 );
-ALTER TABLE ONLY imdb_title_names
-ADD CONSTRAINT imdb_title_names_imdb_title_id_foreign FOREIGN KEY (imdb_title_id) REFERENCES imdb_titles(id) ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY media_names
+ADD CONSTRAINT media_names_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-CREATE TABLE imdb_title_staffs (
+CREATE TABLE media_staffs (
     id BIGSERIAL PRIMARY KEY,
-    imdb_title_id INTEGER NOT NULL,
+    media_id INTEGER NOT NULL,
     name VARCHAR (250) NOT NULL,
     thumbnail TEXT NULL,
     url TEXT NOT NULL,
     role VARCHAR (250) NOT NULL
 );
-ALTER TABLE ONLY imdb_title_staffs
-ADD CONSTRAINT imdb_title_staffs_imdb_title_id_foreign FOREIGN KEY (imdb_title_id) REFERENCES imdb_titles(id) ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY media_staffs
+ADD CONSTRAINT media_staffs_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-CREATE TABLE imdb_title_relations (
+CREATE TABLE media_relations (
     id BIGSERIAL PRIMARY KEY,
-    imdb_title_id INTEGER NOT NULL,
+    media_id INTEGER NOT NULL,
     relation_imdb_id VARCHAR (10) NOT NULL,
     name VARCHAR (250) NOT NULL,
     thumbnail TEXT NULL
 );
-ALTER TABLE ONLY imdb_title_relations
-ADD CONSTRAINT imdb_title_relations_imdb_title_id_foreign FOREIGN KEY (imdb_title_id) REFERENCES imdb_titles(id) ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY media_relations
+ADD CONSTRAINT media_relations_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
