51c77c5f8e7da99822862b9149f5bb823eac4881
Glagan <Glagan@users.noreply.github.com>
2022-07-03 15:42:11 +0200

    [Dev] Remove imdb_id from torrents and rename imdb_title_id to media_id [Fix] Update Media types [Fix] Add missing foreign key relation in Torrent [Dev] Remove Relations tables

--- postgres/init.sql
+++ postgres/init.sql
@@ -25,7 +25,7 @@ VALUES (
 CREATE TABLE torrents (
     id BIGSERIAL PRIMARY KEY,
     full_url TEXT NOT NULL,
-    imdb_title_id INTEGER NULL,
+    media_id INTEGER NULL,
     name VARCHAR (250) NOT NULL,
     type VARCHAR (50) NOT NULL,
     seed INTEGER NULL,
@@ -34,8 +34,7 @@ CREATE TABLE torrents (
     upload_time TIMESTAMP WITH TIME ZONE NULL,
     description_html TEXT NULL,
     torrent_url TEXT NULL,
-    magnet TEXT NULL,
-    imdb_id VARCHAR(10) NULL
+    magnet TEXT NULL
 );
 CREATE UNIQUE INDEX unique_torrent_url ON torrents(full_url);
 
@@ -49,20 +48,35 @@ CREATE TABLE torrent_files (
 ALTER TABLE ONLY torrent_files
 ADD CONSTRAINT torrent_files_torrent_id_foreign FOREIGN KEY (torrent_id) REFERENCES torrents(id) ON DELETE CASCADE NOT DEFERRABLE;
 
--- IMDB Informations
+-- Person Informations
+
+CREATE TABLE names (
+    id BIGSERIAL PRIMARY KEY,
+    tmdb_id INTEGER NOT NULL,
+    name VARCHAR (250) NOT NULL,
+    thumbnail VARCHAR (250) NULL,
+    birth_year INTEGER NULL,
+    death_year INTEGER NULL
+);
+CREATE UNIQUE INDEX unique_name_tmdb_id ON names(tmdb_id);
+
+-- Media Informations
 
 CREATE TABLE medias (
     id BIGSERIAL PRIMARY KEY,
     imdb_id VARCHAR (10) NOT NULL,
+    tmdb_id INTEGER NOT NULL,
     description TEXT NULL,
-    duration INTEGER NOT NULL,
+    duration INTEGER NULL,
     thumbnail TEXT NULL,
     background VARCHAR (250) NULL,
-    year INTEGER NOT NULL,
+    year INTEGER NULL,
     genres VARCHAR (250) NOT NULL,
-    rating DOUBLE NULL
+    rating REAL NULL
 );
 CREATE UNIQUE INDEX unique_media_imdb_id ON medias(imdb_id);
+ALTER TABLE ONLY torrents
+ADD CONSTRAINT torrent_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE SET NULL;
 
 CREATE TABLE media_names (
     id BIGSERIAL PRIMARY KEY,
@@ -93,28 +107,3 @@ CREATE TABLE media_actors (
 ALTER TABLE ONLY media_actors
 ADD CONSTRAINT media_actors_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
 CREATE UNIQUE INDEX unique_media_actor_character ON media_actors(media_id, name_id, character);
-
-CREATE TABLE media_relations (
-    id BIGSERIAL PRIMARY KEY,
-    media_id INTEGER NOT NULL,
-    relation_id INTEGER NOT NULL
-);
-ALTER TABLE ONLY media_relations
-ADD CONSTRAINT media_relations_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
-
-CREATE TABLE names (
-    id BIGSERIAL PRIMARY KEY,
-    imdb_id VARCHAR (10) NOT NULL,
-    name VARCHAR (250) NOT NULL,
-    birth_year INTEGER NULL,
-    death_year INTEGER NULL
-);
-CREATE UNIQUE INDEX unique_name_imdb_id ON names(imdb_id);
-
-CREATE TABLE name_relations (
-    id BIGSERIAL PRIMARY KEY,
-    name_id INTEGER NOT NULL,
-    media_id INTEGER NOT NULL
-);
-ALTER TABLE ONLY name_relations
-ADD CONSTRAINT name_relations_media_id_foreign FOREIGN KEY (media_id) REFERENCES medias(id) ON DELETE CASCADE NOT DEFERRABLE;
