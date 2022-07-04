c224a8ab13655005dc82466571b3e2bd4eca157f
Glagan <Glagan@users.noreply.github.com>
2022-07-04 14:14:25 +0200

    [Dev] Add get single media route

--- sqlc/sql/search/query.sql
+++ sqlc/sql/search/query.sql
@@ -1,8 +1,37 @@
 -- name: GetMediaByID :one
-SELECT *
+SELECT medias.*
 FROM medias
-WHERE id = $1
+WHERE medias.id = $1
 LIMIT 1;
+
+-- name: GetMediaNames :many
+SELECT media_names.*
+FROM media_names
+WHERE media_names.media_id = $1;
+
+-- name: GetMediaTorrents :many
+SELECT torrents.*
+FROM torrents
+RIGHT JOIN medias ON torrents.media_id = medias.id
+WHERE torrents.media_id = $1;
+
+-- name: GetMediaGenres :many
+SELECT genres.*
+FROM media_genres
+RIGHT JOIN genres ON media_genres.genre_id = genres.id
+WHERE media_genres.media_id = $1;
+
+-- name: GetMediaActors :many
+SELECT names.id, names.name, names.thumbnail, media_actors.character
+FROM media_actors
+RIGHT JOIN names ON media_actors.name_id = names.id
+WHERE media_actors.media_id = $1;
+
+-- name: GetMediaStaffs :many
+SELECT names.id, names.name, names.thumbnail, media_staffs.role
+FROM media_staffs
+RIGHT JOIN names ON media_staffs.name_id = names.id
+WHERE media_staffs.media_id = $1;
 
 -- name: GetGenres :many
 SELECT *
