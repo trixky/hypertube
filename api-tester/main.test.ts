4f4c101668155cdb629f04d3f2570f7208560604
Glagan <Glagan@users.noreply.github.com>
2022-07-20 18:25:18 +0200

    [Dev] api-tester: Extract redis connection

--- api-tester/main.test.ts
+++ api-tester/main.test.ts
@@ -1,11 +1,19 @@
+import { connect, disconnect } from './redis/db';
 import { DESCRIBE_TEST_client } from './tests/1.client';
 import { DESCRIBE_TEST_api_auth } from './tests/2.api-auth-internal';
 import { DESCRIBE_TEST_api_user } from './tests/3.api-user';
 import { DESCRIBE_TEST_api_position } from './tests/4.api-position';
 
-jest.setTimeout(15000);
+beforeAll(async () => {
+	await connect();
+});
 
+jest.setTimeout(15000);
 describe('DESCRIBE_TEST_client_external', DESCRIBE_TEST_client);
 describe('DESCRIBE_TEST_api_auth', DESCRIBE_TEST_api_auth);
 describe('DESCRIBE_TEST_api_user', DESCRIBE_TEST_api_user);
 describe('DESCRIBE_TEST_api_position', DESCRIBE_TEST_api_position);
+
+afterAll(async () => {
+	await disconnect();
+});
