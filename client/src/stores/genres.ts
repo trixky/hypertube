8b7f894b316633e4a3036ac7ca5c4cdb4a52f2ab
Glagan <Glagan@users.noreply.github.com>
2022-07-23 16:45:06 +0200

    [Dev] client: Add SSR for search results and genres [Dev] client: Extract get functions in stores

--- client/src/stores/genres.ts
+++ client/src/stores/genres.ts
@@ -6,6 +6,38 @@ export type Genre = {
 	name: string;
 };
 
+export async function getGenres(
+	sender: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch,
+	session?: App.Session
+) {
+	const url = browser
+		? `http://localhost:7072/v1/media/genres`
+		: `http://api-media:7072/v1/media/genres`;
+	const res = await sender(url, {
+		method: 'GET',
+		credentials: 'include',
+		headers: {
+			accept: 'application/json',
+			cookie: !browser ? `token=${session?.token}; locale=${session?.locale}` : ''
+		}
+	});
+	if (res.ok && res.status >= 200 && res.status < 300) {
+		const body = (await res.json()) as {
+			genres: Genre[];
+		};
+		return {
+			response: res,
+			genres: body.genres.sort((a, b) => {
+				return a.name.localeCompare(b.name);
+			})
+		};
+	}
+	return {
+		response: res,
+		genres: []
+	};
+}
+
 export const loaded = writable(false);
 export const loading = writable(true);
 function genresStore() {
@@ -14,35 +46,7 @@ function genresStore() {
 	return {
 		set,
 		subscribe,
-		update,
-		async load(
-			sender: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
-		) {
-			loading.set(true);
-			set([]);
-
-			const url = browser
-				? `http://localhost:7072/v1/media/genres`
-				: `http://api-media:7072/v1/media/genres`;
-			const res = await sender(url, {
-				method: 'GET',
-				credentials: 'include',
-				headers: { accept: 'application/json' }
-			});
-			if (res.ok) {
-				const body = (await res.json()) as {
-					genres: Genre[];
-				};
-				set(
-					body.genres.sort((a, b) => {
-						return a.name.localeCompare(b.name);
-					})
-				);
-				loaded.set(true);
-			}
-
-			loading.set(true);
-		}
+		update
 	};
 }
 
