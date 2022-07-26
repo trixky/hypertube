2556b4add0dd21be8c98626a15f16b626e58d050
trixky <mabois@student.42.fr>
2022-07-26 15:19:20 +0200

    [Fix] css: fix the css of the auth pages

--- client/src/routes/login/continue-with.svelte
+++ client/src/routes/login/continue-with.svelte
@@ -9,12 +9,12 @@
 </script>
 
 <!-- ========================= HTML -->
-<a {href}>
+<a class="w-full text-center" {href}>
 	<button
-		class="inline-block bg-white py-2 px-3 rounded-full duration-[0.35s] hover:bg-gray-300 disabled:opacity-30"
+		class="inline-block bg-white w-full py-2 px-3 rounded-full duration-[0.35s] hover:bg-gray-300 disabled:opacity-30"
 		{disabled}
 	>
 		{$_('auth.continue_with')}
-		<img class="inline-block ml-[1px]" alt="{destination} logo" src={logo} height="20" width="20" />
+		<img class="inline-block ml-[2px]" alt="{destination} logo" src={logo} height="20" width="20" />
 	</button>
 </a>
