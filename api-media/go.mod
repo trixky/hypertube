f67df4815ee0cdbd65cbca8533d3176c90bb57f8
Glagan <Glagan@users.noreply.github.com>
2022-07-28 15:19:42 +0200

    [Dev] test-go: Add tests for api-media and api-scrapper [Dev] test-go: Ignore generated folders in tests

--- api-media/go.mod
+++ api-media/go.mod
@@ -8,13 +8,13 @@ require (
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/golang/protobuf v1.5.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.0
-	github.com/lib/pq v1.10.6
 	google.golang.org/genproto v0.0.0-20220719170305-83ca9fad585f
 	google.golang.org/grpc v1.48.0
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
+	github.com/lib/pq v1.10.6 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.19.0 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
---
+++ api-media/internal/search.handler_test.go
@@ -0,0 +1,149 @@
+package internal
+
+import (
+	"context"
+	"testing"
+
+	"github.com/trixky/hypertube/.shared/databases"
+	"github.com/trixky/hypertube/.shared/environment"
+	_test "github.com/trixky/hypertube/.shared/test"
+	"github.com/trixky/hypertube/api-media/proto"
+	"github.com/trixky/hypertube/api-media/queries"
+	"google.golang.org/grpc/metadata"
+)
+
+func init() {
+	// Set environment config
+	environment_config := environment.Config{
+		ENV_grpc_port:         "API_AUTH_GRPC_PORT",
+		ENV_grpc_gateway_port: "API_AUTH_GRPC_GATEWAY_PORT",
+		ENV_http_port:         "API_AUTH_HTTP_PORT",
+	}
+
+	environment.Postgres.GetAll()                // Get postgres environment
+	environment.Redis.GetAll()                   // Get redis environment
+	environment.Grpc.GetAll(&environment_config) // Get grpc environment
+	environment.Http.GetAll(&environment_config) // Get http environment
+
+	databases.InitPostgres() // Init postgres
+	databases.InitRedis()    // Init redis
+	queries.InitSqlc()       // Init sqlc queries
+}
+
+func TestSearch(t *testing.T) {
+	server := &MediaServer{}
+
+	query := "42"
+	var page uint32 = 1
+	year := "year"
+	var yearNumber uint32 = 199
+	sortAsc := "asc"
+	var rating float32 = 7.2
+
+	tests := []struct {
+		token           string
+		input           *proto.SearchRequest
+		corrupted_token bool
+		invalid_token   bool
+		error_expected  bool
+	}{
+		// ------------------------- Failed expected
+		{ // Token missing #1
+			input:          &proto.SearchRequest{},
+			error_expected: true,
+		},
+		{ // Token invalid
+			token:          "576100d0-0c2b-11ed-861d-0242ac120002",
+			input:          &proto.SearchRequest{},
+			invalid_token:  true,
+			error_expected: true,
+		},
+		{ // Token corrupted
+			token:           "528ff7b4-0c2b-11ed-861d-0242ac120002",
+			input:           &proto.SearchRequest{},
+			corrupted_token: true,
+			error_expected:  true,
+		},
+		// ------------------------- Success expected
+		{
+			token:          "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input:          &proto.SearchRequest{},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				SortBy: &year,
+			},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				SortOrder: &sortAsc,
+			},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				Year: &yearNumber,
+			},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				Rating: &rating,
+			},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				Query: &query,
+			},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				GenreIds: []int32{1, 2},
+			},
+			error_expected: false,
+		},
+		{
+			token: "f944c98c-0c2a-11ed-861d-0242ac120002",
+			input: &proto.SearchRequest{
+				Query:     &query,
+				Page:      &page,
+				SortBy:    &year,
+				SortOrder: &sortAsc,
+				Year:      &yearNumber,
+				Rating:    &rating,
+				GenreIds:  []int32{1, 2},
+			},
+			error_expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		ctx := context.Background()
+
+		if len(test.token) > 0 {
+			if test.invalid_token {
+				test.token = _test.InvalidateToken(test.token)
+			} else if test.corrupted_token {
+				test.token = _test.CorrupteToken(test.token)
+			}
+
+			ctx = metadata.NewIncomingContext(ctx, metadata.New(map[string]string{
+				"grpcgateway-cookie": "token=" + test.token + ";",
+			}))
+		}
+
+		if _, err := server.Search(ctx, test.input); (err != nil) != test.error_expected {
+			t.Fatal(err)
+		}
+	}
+}
