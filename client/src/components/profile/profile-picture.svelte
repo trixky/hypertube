337ed96940c7d2e4ff9d049d27533b104305bde9
trixky <mabois@student.42.fr>
2022-07-29 16:02:44 +0200

    [Dev] profile picture front: finish to add profile picture to the front (not connected to the back)

---
+++ client/src/components/profile/profile-picture.svelte
@@ -0,0 +1,21 @@
+<!-- ========================= SCRIPT -->
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+
+    export let user_id: number;
+</script>
+
+<!-- ========================= HTML -->
+<a href="/users/{user_id}">
+	<img
+		class="inline bg-blue-300 rounded-[10px]"
+		src="/user.png"
+		width="20px"
+		height="20px"
+		alt={$_('auth.my_profile')}
+	/>
+</a>
+
+<!-- ========================= CSS -->
+<style lang="postcss">
+</style>
