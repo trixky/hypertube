f0c8aa5f2eac66f94e4b3e209e578ddaea1ab6a7
Mathis Bois <mathis.bois@outlook.fr>
2022-07-30 17:03:38 +0200

    [Dev] picture profile: connect the GET methode to the front

--- client/src/components/profile/profile-picture.svelte
+++ client/src/components/profile/profile-picture.svelte
@@ -3,15 +3,16 @@
 	import { _ } from 'svelte-i18n';
 
     export let user_id: number;
+    export let size = 20;
 </script>
 
 <!-- ========================= HTML -->
 <a href="/users/{user_id}">
 	<img
-		class="inline bg-blue-300 rounded-[10px]"
-		src="/user.png"
-		width="20px"
-		height="20px"
+		class="inline rounded-full"
+		src="http://localhost:5180/users/{user_id}/picture"
+		width="{size}px"
+		height="{size}px"
 		alt={$_('auth.my_profile')}
 	/>
 </a>
