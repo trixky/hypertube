37e94dc8c8af63f762636f3641efbc1576a64ed0
trixky <mabois@student.42.fr>
2022-08-16 15:54:47 +0200

    [Dev] background: finish dynamic background

---
+++ client/src/components/animations/movie.svelte
@@ -0,0 +1,20 @@
+<!-- ========================= SCRIPT -->
+<script lang="ts">
+    export let index: number;
+    export let movie: string;
+
+    let flip = false;
+</script>
+
+<!-- ========================= HTML -->
+<div class="movie inline-block w-[120px] h-[180px]" style="transform: translateX({(index * 40) % 120}px); background-image: url({movie});"  alt="" />
+
+<!-- ========================= CSS -->
+<style lang="postcss">
+    .movie {
+        margin: 0 3px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
+</style>
