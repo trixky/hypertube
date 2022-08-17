6f4aa61d3b13aa116ddc44b4d3f48750f07b927e
trixky <mabois@student.42.fr>
2022-08-17 19:33:56 +0200

    [Dev] demo: improve demo mode x2

--- client/src/routes/login/continue-with.svelte
+++ client/src/routes/login/continue-with.svelte
@@ -6,13 +6,18 @@
 	export let logo = '';
 	export let disabled = false;
 	export let href: string;
+
+	const demo_mode = import.meta.env.VITE_DEMO_MODE;
+
+	$: _disabled = disabled || demo_mode != 'false'
 </script>
 
 <!-- ========================= HTML -->
-<a class="w-full text-center overflow-hidden" {href}>
+<a class="w-full text-center overflow-hidden" href={_disabled ? '#' : href}>
 	<button
+		class:cursor-not-allowed={_disabled}
 		class="inline-block bg-white w-full py-2 px-3 rounded-full duration-[0.35s] hover:bg-gray-300 disabled:opacity-30 text-center"
-		{disabled}
+		disabled={_disabled}
 	>
 		<div class="flex justify-center items-center">
 			<p class="truncate pr-2 hidden md:block">{$_('auth.continue_with')}</p>
